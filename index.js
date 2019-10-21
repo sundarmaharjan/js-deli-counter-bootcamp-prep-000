49dfb59ba276edee022d499d3c1fc515b640f1e5
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-21 16:42:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(KatzDeliLine) {
   if(KatzDeliLine.length === 0 ){
     return "there is nobody is waiting to be served, Mate!!"
   } else {
-    var name = newcustomer [0];
+    var name = customer [0];
     KatzDeliLine.splice(0,1)
     
     
