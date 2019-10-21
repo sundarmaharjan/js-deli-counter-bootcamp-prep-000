2d05a765076d2d351671e14e086f16b679de39e8
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-21 16:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var KatzDeliLine = [];
 
 function takeANumber(KatzDeliLine, name) {
   KatzDeliLine.push(name);
-  return "welcome " + name + " . you are number 1 " + KatzDeliLine.length + "in-line." ;
+  return "welcome " + name + " . you are number 1 " + KatzDeliLine.length + "in line." ;
 }
 
